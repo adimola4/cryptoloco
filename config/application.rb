a0f5f32d238ceae9dd75567a77212311df108a2d
adimola4 <adimola4@gmail.com>
2021-06-24 16:05:19 -0400

    fix config

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module NotifyArticles
+module Cryptoloco
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
@@ -47,3 +47,6 @@ module NotifyArticles
     config.generators.system_tests = nil
   end
 end
+
+
+
